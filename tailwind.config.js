e88e49f5cc74dee681411d5d048020a540740730
Melodi Balossino <melodibalossino@gmail.com>
2024-03-14 23:55:00 -0300

    Termina el Header en mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",                    // Agregar clase para el dark mode
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
